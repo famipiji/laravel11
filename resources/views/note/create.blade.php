84a9c03921c70335d4d790966701edc4b38d508c
famipiji <fahmihafizi40@gmail.com>
2024-07-19 10:20:43 +0800

    first commit

---
+++ resources/views/note/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1>create</h1>
+    <p>hello</p>
+    <img src="https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg">
+
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+use App\Http\Controllers\NoteController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+/*Route::get('/note',[NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create',[NoteController::class, 'create'])->name('note.create');
+Route::get('/note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');*/
+
+
+Route::resource('note', NoteController::class);//<-this code covers all the code above
